6b9662a47bdca50d48b3321db7008378da19ffed
Radium <2043869032@qq.com>
2024-05-18 22:17:45 +0800

    feat: init probject

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn_admin_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+    }
+}
