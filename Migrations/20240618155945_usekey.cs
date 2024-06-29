6d4736974d0f3506d7fe164133ae6d1b86aca39a
Radium <2043869032@qq.com>
2024-06-29 20:51:03 +0800

    chore: op code

---
+++ Migrations/20240618155945_usekey.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace learn_admin_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class usekey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Account = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pdfs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Url = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserKey = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pdfs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pdfs_Users_UserKey",
+                        column: x => x.UserKey,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pdfs_UserKey",
+                table: "Pdfs",
+                column: "UserKey");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pdfs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Share/AliyunOss.cs
@@ -0,0 +1,41 @@
+﻿using Aliyun.OSS;
+using Aliyun.OSS.Common;
+using System.Security.Cryptography;
+
+namespace learn_admin_backend.Share
+{
+    public class AliyunOss
+    {
+        string endpoint;
+        string accessKeyId;
+        string accessKeySecret;
+        OssClient client;
+        readonly IConfiguration configuration;
+        AutoResetEvent _event = new AutoResetEvent(false);
+        HashAlgorithm hashAlgorithm = new MD5CryptoServiceProvider();
+
+        public AliyunOss(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.endpoint = configuration["Endpoint"];
+            this.accessKeyId = configuration["accessKeyId"];
+            this.accessKeySecret = configuration["AccessKeySecret"];
+            this.client = new OssClient(this.endpoint, this.accessKeyId, this.accessKeySecret);
+        }
+
+        public void PutObjectFromFile(string bucketName, Stream fileToUpload)
+        {
+            const string key = "PutObjectFromFile";
+            try
+            {
+                client.PutObject(bucketName, key, fileToUpload);
+            }
+            catch (OssException ex)
+            {
+                Console.WriteLine("Failed with error code: {0}; Error info: {1}. \nRequestID:{2}\tHostID:{3}",
+                    ex.ErrorCode, ex.Message, ex.RequestId, ex.HostId);
+            }
+        }
+
+    }
+}
