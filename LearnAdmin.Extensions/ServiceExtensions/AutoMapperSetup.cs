6d40182e136703f1ad6898eb9f7dd893f43b237b
Radium <2043869032@qq.com>
2024-07-19 21:49:03 +0800

    chore: op code

---
+++ LearnAdmin.Extensions/ServiceExtensions/AutoMapperSetup.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LearnAdmin.Extensions.ServiceExtensions
+{
+    public static class AutoMapperSetup
+    {
+        public static void AddAutoMapperSetup(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            services.AddAutoMapper(typeof(AutoMapperConfig));
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+
+    public class AutoMapperConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new UserInfoProfile());
+            });
+        }
+    }
+}
---
+++ LearnAdmin.Extensions/UserInfoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace LearnAdmin.Extensions
+{
+    public class UserInfoProfile : Profile
+    {
+        /// <summary>
+        /// 配置构造函数，用来创建关系映射
+        /// </summary>
+        public UserInfoProfile()
+        {
+        }
+    }
+}
